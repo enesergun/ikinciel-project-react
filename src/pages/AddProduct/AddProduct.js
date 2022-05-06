aac9308f8317dbe04267f01dff510239deecd28e
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-05-06 16:49:15 +0300

    all selects convert Field component

--- src/pages/AddProduct/AddProduct.js
+++ src/pages/AddProduct/AddProduct.js
@@ -12,12 +12,63 @@ import Previews from '../../components/DragAndDrop/DragAndDrop';
 import Select from 'react-select';
 
 
+const customStyles = {
+  indicatorSeparator: (provided) => ({
+    display: 'none',
+  }),
+  input: (provided) => ({
+    margin: 0,
+  }),
+  container: (provided, state) => ({
+    ...provided,
+    /* height: 40, */
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: '#ccc',
+    height: 40,
+  })
+  
+  /* option: (provided, state) => ({
+    ...provided,
+    fontWeight: state.isSelected ? "bold" : "normal",
+    color: "black",
+    backgroundColor: 'white',
+    fontSize: 20
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: 'green',
+    fontSize: 20
+  }) */
+}
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' },
 ];
+const optionsBrand = [
+  { value: 'marka1', label: 'marka1' },
+  { value: 'marka2', label: 'marka2' },
+  { value: 'marka3', label: 'marka3' },
+];
+
+const optionsColor = [
+  { value: 'renk1', label: 'renk1' },
+  { value: 'renk2', label: 'renk2' },
+  { value: 'renk3', label: 'renk3' },
+];
+
+
+const optionsStatus = [
+  { value: 'status1', label: 'status1' },
+  { value: 'status2', label: 'status2' },
+  { value: 'status3', label: 'status3' },
+];
+
+
+
 
 
 function AddProduct() {
@@ -97,8 +148,7 @@ function AddProduct() {
                               name='category'
                               options={options}
                               component={MySelect}
-                              placeholder="Kategori Seçiniz"
-                              isMulti={false}
+                              placeholder="Kategori Seçiniz"                              
                             />
                             
                               
@@ -109,19 +159,14 @@ function AddProduct() {
                           
                           <label className={styles.AddProductLabel}>Marka</label>
 
-                          <select 
-                                className={styles.selectWrapper}
-                                name="brand"                      
-                                value={values.brand}
-                                onChange={handleChange}
-                                onBlur={handleBlur} 
-                                >
-
-                                <option value="mavi" label='mavi'>mavi</option>
-                                <option value="koton" label='koton'>koton</option>
-                                <option value="lcw" label='lcw'>lcw</option>
-                                <option value="nike" label='nike'>nike</option>
-                          </select>
+                          
+                            <Field
+                                className='custom-select'
+                                name='brand'
+                                options={optionsBrand}
+                                component={MySelect}
+                                placeholder="Marka Seçiniz"                              
+                            />
 
                           </div>
                         </div>
@@ -131,38 +176,30 @@ function AddProduct() {
                             
                             <label className={styles.AddProductLabel}>Renk</label>
 
-                            <select 
-                                className={styles.selectWrapper}
-                                name="color"                      
-                                value={values.color}
-                                onChange={handleChange}
-                                onBlur={handleBlur} 
-                                >
+                           
 
-                                <option value="yeşil" label='yeşil'>yeşil</option>
-                                <option value="sarı" label='sarı'>sarı</option>
-                                <option value="turuncu" label='turuncu'>turuncu</option>
-                                <option value="kırmızı" label='kırmızı'>kırmızı</option>
-                              </select>
+                            <Field
+                              className='custom-select'
+                              name='color'
+                              options={optionsColor}
+                              component={MySelect}
+                              placeholder="Renk Seçiniz"                              
+                            />
+
 
                           </div>
                           <div className={`${styles.condition} ${styles.formGroup}`}>
 
                             <label className={styles.AddProductLabel}>Kullanım Durumu</label>
 
-                            <select 
-                                className={styles.selectWrapper}
-                                name="condition"                      
-                                value={values.condition}
-                                onChange={handleChange}
-                                onBlur={handleBlur} 
-                                >
-
-                                <option value="güzel" label='güzel'>güzel</option>
-                                <option value="idare eder" label='idare eder'>idare eder</option>
-                                <option value="harika" label='harika'>harika</option>
-                                <option value="pis" label='pis'>pis</option>
-                              </select>
+                           
+                               <Field
+                                className='custom-select'
+                                name='condition'
+                                options={optionsStatus}
+                                component={MySelect}
+                                placeholder="Kullanım durumu seçiniz"
+                            />
                           </div>
                         </div>
                         <div className={`${styles.price} ${styles.formGroup}`}>
@@ -217,7 +254,7 @@ export default AddProduct
 
 
 
-function MySelect({className, name, options, field, form, isMulti= false, placeholder}) {
+function MySelect({className, name, options, field, form, placeholder}) {
 
   const onChange = (option) => {
     form.setFieldValue(field.name, option.value)
@@ -227,108 +264,14 @@ function MySelect({className, name, options, field, form, isMulti= false, placeh
     <>
       <Select
           className={className}
-          name={field.name}
-          /* value={getValue()} */
+          name={field.name}          
           onChange={onChange}
           placeholder={placeholder}
-          options={options}
-          isMulti={isMulti}
+          options={options}  
+          styles={customStyles}
       />      
     </>
   )
   
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <div className={styles.productDetails}>
-                <div className={styles.header}>Ürün Detayları</div>
-                <div className={styles.addProductForm}>
-                  <div className={`${styles.productName} ${styles.formGroup}`}>
-                    <label className={styles.AddProductLabel}>Ürün Adı</label>
-                    <input type="text" />
-                  </div>
-                  <div className={`${styles.description} ${styles.formGroup}`}>
-                    <label className={styles.AddProductLabel}>Açıklama</label>
-                    <input type="text" />
-                  </div>
-
-                  <div className={styles.SelectGroup}>
-                    <div className={`${styles.category} ${styles.formGroup}`}>
-                      <label className={styles.AddProductLabel}>Kategori</label>
-
-                      
-                        <select name="categories" className={styles.selectWrapper}>
-                          <option value="pantol">pantol</option>
-                          <option value="tişmört">tişmört</option>
-                          <option value="kazak">kazak</option>
-                          <option value="ceket">ceket</option>
-                        </select>
-                      
-                    </div>
-                    <div className={`${styles.brand} ${styles.formGroup}`}>
-                    
-                    <label className={styles.AddProductLabel}>Marka</label>
-
-                      <select name="brand" id="">
-                        <option value="mavi">mavi</option>
-                        <option value="lcw">lcw</option>
-                        <option value="koton">koton</option>
-                        <option value="nike">nike</option>
-                      </select>
-
-                    </div>
-                  </div>
-
-                  <div className={styles.SelectGroup}>
-                    <div className={`${styles.color} ${styles.formGroup}`}>
-                      
-                      <label className={styles.AddProductLabel}>Renk</label>
-
-                      <select name="colors" id="">
-                        <option value="mavi">mavi</option>
-                        <option value="yeşil">yeşil</option>
-                        <option value="sarı">sarı</option>
-                        <option value="kırmızı">kırmızı</option>
-                      </select>
-
-                    </div>
-                    <div className={`${styles.condition} ${styles.formGroup}`}>
-
-                      <label className={styles.AddProductLabel}>Kullanım Durumu</label>
-
-                      <select name="contion" id="">
-                        <option value="five">five</option>
-                        <option value="four">four</option>
-                        <option value="three">three</option>
-                        <option value="two">two</option>
-                      </select>
-
-                    </div>
-                  </div>
-                  <div className={`${styles.price} ${styles.formGroup}`}>
-                    <label className={styles.AddProductLabel}>Fiyat</label>
-                    <input type="number" name="price" id="" />
-                  </div>
-                  <div className={styles.offerable}>
-                  <label class={styles.switch}>
-                    offerable
-                    <input type="checkbox" />
-                    <span class={`${styles.slider} ${styles.round}`}></span>                    
-                  </label>                                                          
-                  </div>
-                </div>
-              </div> */
